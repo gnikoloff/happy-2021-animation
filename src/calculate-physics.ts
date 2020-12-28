98c213af58392d80b9429d70ea84ed55bba64ced
Georgi Nikolov <nikoloffgeorgi@gmail.com>
2020-12-28 18:55:12 +0100

    implemented lines collisions

--- src/calculate-physics.ts
+++ src/calculate-physics.ts
@@ -2,126 +2,159 @@ let oldTime = 0
 
 const calculatePhysics = ({
   velocitiesArray,
-  offsetsArray
+  oldOffsetsArray,
+  offsetsArray,
+  linesOffsetsArray,
+  linesRotationsArray,
+  linesVertexArray,
 }, {
   particlesCount,
   gravity,
   radius,
   bounceScale,
   innerWidth,
-  innerHeight
+  innerHeight,
 }) => {
   const now = performance.now()
   const dt = now - oldTime
-  oldTime = now
+  oldTime = dt
+
+  const getLineBounds = i => {
+    const x1 = linesVertexArray[0]
+    const y1 = linesVertexArray[1]
+    const x2 = linesVertexArray[2]
+    const y2 = linesVertexArray[3]
+    if (linesRotationsArray[i] === 0) {
+      const minX = Math.min(x1, x2)
+      const minY = Math.min(y1, y2)
+      const maxX = Math.max(x1, x2)
+      const maxY = Math.max(y1, y2)
+      return {
+        x: x1 + minX,
+        y: y1 + minY,
+        width: maxX - minX,
+        height: maxY - minY,
+      }
+    } else {
+      const rotation = linesRotationsArray[i]
+      const sin = Math.sin(rotation)
+      const cos = Math.cos(rotation)
+      const x1r = cos * x1 + sin * y1
+      const x2r = cos * x2 + sin * y2
+      const y1r = cos * y1 + sin * x1
+      const y2r = cos * y2 + sin * x2
+      const x = linesOffsetsArray[i * 2 + 0] + x1 + Math.min(x1r, x2r)
+      const y = linesOffsetsArray[i * 2 + 1] + y1 + Math.min(y1r, y2r)
+      const width = Math.max(x1r, x2r) - Math.min(x1r, x2r)
+      const height = Math.max(y1r, y2r) - Math.min(y1r, y2r)
+      return {
+        x,
+        y,
+        width,
+        height,
+      }
+    }
+  }
+
+  const move = i => {
+    velocitiesArray[i * 2 + 1] += gravity
+    offsetsArray[i * 2 + 0] += velocitiesArray[i * 2 + 0] * (dt * 0.01)
+    offsetsArray[i * 2 + 1] += velocitiesArray[i * 2 + 1] * (dt * 0.01)
+  }
 
   const checkWall = i => {
     if (offsetsArray[i * 2 + 0] < radius / 2) {
       offsetsArray[i * 2] = radius / 2
       velocitiesArray[i * 2] *= -bounceScale
-    } else if (offsetsArray[i * 2] + radius / 2 > innerWidth) {
+    }
+    if (offsetsArray[i * 2] + radius / 2 > innerWidth) {
       offsetsArray[i * 2] = innerWidth - radius / 2
       velocitiesArray[i * 2] *= -bounceScale
     }
 
-    if (offsetsArray[i * 2 + 1] + radius / 2 > innerHeight) {
-      offsetsArray[i * 2 + 1] = innerHeight - radius / 2
-      velocitiesArray[i * 2 + 1] *= -bounceScale
-
-      // offsetsArray[i * 2 + 1] = Math.random() * -innerHeight
+    // if (offsetsArray[i * 2 + 1] < radius / 2) {
+    //   offsetsArray[i * 2 + 1] = radius / 2
+    //   velocitiesArray[i * 2 + 1] *= -bounceScale
+    // }
+    if (offsetsArray[i * 2 + 1] - radius / 2 > innerHeight) {
+      offsetsArray[i * 2 + 1] = -radius
+      velocitiesArray[i * 2 + 1] = 0
     }
   }
-  const rotate = (x, y, sin, cos, reverse) => {
-    return {
-      x: reverse ? x * cos + y * sin : x * cos - y * sin,
-      y: reverse ? y * cos - x * sin : y * cos + x * sin
-    }
-  }
-  const checkCollision = (i0, i1) => {
-    // debugger
-    const ball0x = offsetsArray[i0 * 2 + 0]
-    const ball0y = offsetsArray[i0 * 2 + 1]
-    const ball1x = offsetsArray[i1 * 2 + 0]
-    const ball1y = offsetsArray[i1 * 2 + 1]
 
-    const ball0vx = velocitiesArray[i0 * 2 + 0]
-    const ball0vy = velocitiesArray[i0 * 2 + 1]
-    const ball1vx = velocitiesArray[i1 * 2 + 0]
-    const ball1vy = velocitiesArray[i1 * 2 + 1]
+  const checkLine = lineIdx => {
+    const lineBounds = getLineBounds(lineIdx)
 
-    const dx = ball1x - ball0x
-    const dy = ball1y - ball0y
-    const dist = Math.sqrt(dx * dx + dy * dy)
-    const minDist = radius / 2 + radius / 2
+    for (let i = 0; i < particlesCount; i++) {
 
-    if (dist < minDist) {
-      //calculate angle, sine, and cosine
-      const angle = Math.atan2(dy, dx)
-      const sin = Math.sin(angle)
-      const cos = Math.cos(angle)
 
-      //rotate ball0's position
-      const pos0 = { x: 0, y: 0 }
+      const ballx = offsetsArray[i * 2 + 0]
+      const bally = offsetsArray[i * 2 + 1]
+      const ballvx = velocitiesArray[i * 2 + 0]
+      const ballvy = velocitiesArray[i * 2 + 1]
 
-      //rotate ball1's position
-      const pos1 = rotate(dx, dy, sin, cos, true)
+      // debugger
+      if (ballx + radius / 2 > lineBounds.x && ballx - radius / 2 < lineBounds.x + lineBounds.width) {
+        // debugger
+        const lineRotation = linesRotationsArray[lineIdx]
+        const cos = Math.cos(lineRotation)
+        const sin = Math.sin(lineRotation)
 
-      //rotate ball0's velocity
-      const vel0 = rotate(ball0vx, ball0vy, sin, cos, true)
+        let x = ballx - linesOffsetsArray[lineIdx * 2 + 0]
+        let y = bally - linesOffsetsArray[lineIdx * 2 + 1]
+        let vx1 = cos * ballvx + sin * ballvy
+        let vy1 = cos * ballvy - sin * ballvx
 
-      //rotate ball1's velocity
-      const vel1 = rotate(ball1vx, ball1vy, sin, cos, true)
+        let y1 = cos * y - sin * x
 
-      //collision reaction
-      const vxTotal = (vel0.x - vel1.x)
+        if (y1 > -radius / 2 && y1 < vy1) {
+          // console.log('----------------')
+          // console.log(lineBounds.x, lineBounds.x + lineBounds.width)
+          // console.log(ballx + radius, ballx - radius)
+          const x2 = cos * x + sin * y
 
-      const ball0 = { mass: 1 }
-      const ball1 = { mass: 1 }
+          y1 = -radius / 2
+          vy1 *= -0.35
 
-      vel0.x = ((ball0.mass - ball1.mass) * vel0.x + 2 * ball1.mass * vel1.x) /
-        (ball0.mass + ball1.mass)
-      vel1.x = vxTotal + vel0.x
+          x = cos * x2 - sin * y1
+          y = cos * y1 + sin * x2
 
-      const absV = Math.abs(vel0.x) + Math.abs(vel1.x)
-      const overlap = (radius / 2 + radius / 2) - Math.abs(pos0.x - pos1.x)
-      pos0.x += vel0.x / absV * overlap
-      pos1.x += vel1.x / absV * overlap
+          velocitiesArray[i * 2 + 0] = cos * vx1 - sin * vy1
+          velocitiesArray[i * 2 + 1] = cos * vy1 + sin * vx1
 
-      //rotate positions back
-      const pos0F = rotate(pos0.x, pos0.y, sin, cos, false)
-      const pos1F = rotate(pos1.x, pos1.y, sin, cos, false)
-
-      //adjust positions to actual screen positions
-      offsetsArray[i0 * 2 + 0] = ball0x + pos0F.x
-      offsetsArray[i0 * 2 + 1] = ball0y + pos0F.y
-      offsetsArray[i1 * 2 + 0] = ball0x + pos1F.x
-      offsetsArray[i1 * 2 + 1] = ball0y + pos1F.y
-
-      //rotate velocities back
-      const vel0F = rotate(vel0.x, vel0.y, sin, cos, false)
-      const vel1F = rotate(vel1.x, vel1.y, sin, cos, false)
-
-      velocitiesArray[i0 * 2 + 0] = vel0F.x
-      velocitiesArray[i0 * 2 + 1] = vel0F.y
-      velocitiesArray[i1 * 2 + 0] = vel1F.x
-      velocitiesArray[i1 * 2 + 1] = vel1F.y
+          offsetsArray[i * 2 + 0] = linesOffsetsArray[lineIdx * 2 + 0] + x
+          offsetsArray[i * 2 + 1] = linesOffsetsArray[lineIdx * 2 + 1] + y
+          if (isNaN(linesOffsetsArray[i * 2 + 0] + x) || isNaN(linesOffsetsArray[i * 2 + 1] + y)) {
+            // debugger
+          }
+        }
+        if (
+          isNaN(velocitiesArray[i * 2 + 0]) ||
+          isNaN(velocitiesArray[i * 2 + 1]) ||
+          isNaN(offsetsArray[i * 2 + 0]) ||
+          isNaN(offsetsArray[i * 2 + 1])
+        ) {
+          // debugger
+        }
+      }
     }
   }
 
   for (let i = 0; i < particlesCount; i++) {
-    velocitiesArray[i * 2 + 1] += gravity
-    offsetsArray[i * 2 + 0] += velocitiesArray[i * 2 + 0] * (dt * 0.005)
-    offsetsArray[i * 2 + 1] += velocitiesArray[i * 2 + 1] * (dt * 0.005)
+    move(i)
     checkWall(i)
   }
 
-  for (let i = 0; i < particlesCount; i++) {
-    for (let j = i + 1; j < particlesCount; j++) {
-      checkCollision(i, j)
-    }
+  for (let i = 0; i < linesRotationsArray.length; i++) {
+    checkLine(i)
   }
 
-  return { velocitiesArray, offsetsArray }
+  // console.log(lineBounds.x, lineBounds.x + lineBounds.width)
+
+
+
+
+  return { velocitiesArray, offsetsArray, oldOffsetsArray }
 }
 
 export default calculatePhysics
