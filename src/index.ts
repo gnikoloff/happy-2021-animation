55a4b7d18ae6a8e25b4d2f2def91e7b6eff5722c
Georgi Nikolov <nikoloffgeorgi@gmail.com>
2020-12-28 20:40:34 +0100

    move physics math into index.ts

--- src/index.ts
+++ src/index.ts
@@ -27,15 +27,15 @@ import {
 const GLOBAL_STATE = {
   innerWidth,
   innerHeight,
-  radius: 50,
-  particleCount: 50,
-  linesCount: 20,
+  radius: 200,
+  particleCount: 100,
+  linesCount: 3,
+  lineWidth: 300,
   bounceScale: 0.8,
   gravity: 0.005,
   useWorker: false,
 }
 
-const threadChooserWrapper = document.getElementsByClassName('thread-options')[0]
 const appContainer = document.getElementById('canvas-container')
 const canvas = document.createElement('canvas')
 const gl = canvas.getContext('webgl')
@@ -48,13 +48,14 @@ const vaoExtension = getExtension(gl, 'OES_vertex_array_object')
 
 let u_targetTexture
 let u_textTexture
+let oldTime = 0
 
 // ------- Fullscreen quad program and geometry -------
 const planeProgram = makeProgram(gl, {
   vertexShaderSource: quadVertexShaderSource,
   fragmentShaderSource: quadFragmentShaderSource,
 })
-webglDebugExtension.tagObject(planeProgram, 'planeProgram')
+// webglDebugExtension.tagObject(planeProgram, 'planeProgram')
 
 const planeVertexArray = new Float32Array([1.0, 1.0, -1.0, 1.0, -1.0, -1.0, -1.0, -1.0, 1.0, -1.0, 1.0, 1.0])
 const planeUvsArray = new Float32Array([1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1])
@@ -69,7 +70,7 @@ const planeUvBuffer = gl.createBuffer()
 
 // Create quad VAO and bind it
 const planeVao = vaoExtension.createVertexArrayOES()
-webglDebugExtension.tagObject(planeVao, 'planeVao')
+// webglDebugExtension.tagObject(planeVao, 'planeVao')
 vaoExtension.bindVertexArrayOES(planeVao)
 
 // Quad vertices
@@ -92,14 +93,31 @@ const linesProgram = makeProgram(gl, {
   vertexShaderSource: linesVertexShaderSource,
   fragmentShaderSource: linesFragmentShaderSource,
 })
-const linesVertexArray = new Float32Array([0, 0, 300, 0])
+const linesVertexArray = new Float32Array([0, 0, GLOBAL_STATE.lineWidth, 0])
 const linesOffsetsArray = new Float32Array(GLOBAL_STATE.linesCount * 2)
 const linesRotationsArray = new Float32Array(GLOBAL_STATE.linesCount)
 for (let i = 0; i < GLOBAL_STATE.linesCount; i++) {
-  linesOffsetsArray[i * 2 + 0] = Math.random() * innerWidth
-  linesOffsetsArray[i * 2 + 1] = Math.random() * innerHeight
 
-  linesRotationsArray[i] = (Math.random() * 2 - 1) * 45 * Math.PI / 180
+  const maxLineOccupyHeight = innerHeight * 0.75
+
+  const lineStepY = maxLineOccupyHeight / GLOBAL_STATE.linesCount
+
+  let angle
+  if (i === 0) {
+    linesOffsetsArray[i * 2 + 0] = innerWidth / 2 - innerWidth / 5 - GLOBAL_STATE.lineWidth / 2
+    linesOffsetsArray[i * 2 + 1] = lineStepY
+    angle = 15
+  } else if (i === 1) {
+    linesOffsetsArray[i * 2 + 0] = innerWidth / 2 + innerWidth / 5 - GLOBAL_STATE.lineWidth / 2
+    linesOffsetsArray[i * 2 + 1] = lineStepY * 2
+    angle = -15
+  } else if (i === 2) {
+    linesOffsetsArray[i * 2 + 0] = innerWidth / 2 - innerWidth / 8 - GLOBAL_STATE.lineWidth / 2
+    linesOffsetsArray[i * 2 + 1] = lineStepY * 3
+    angle = 15
+  }
+
+  linesRotationsArray[i] = angle * Math.PI / 180
 }
 
 // Look up lines program attributes
@@ -109,15 +127,15 @@ const lineRotationLocation = gl.getAttribLocation(linesProgram, 'a_rotation')
 
 // Create lines buffers
 const linesVertexBuffer = gl.createBuffer()
-webglDebugExtension.tagObject(linesVertexBuffer, 'linesVertexBuffer')
+// webglDebugExtension.tagObject(linesVertexBuffer, 'linesVertexBuffer')
 const linesOffsetsBuffer = gl.createBuffer()
-webglDebugExtension.tagObject(linesOffsetsBuffer, 'linesOffsetsBuffer')
+// webglDebugExtension.tagObject(linesOffsetsBuffer, 'linesOffsetsBuffer')
 const linesRotationBuffers = gl.createBuffer()
-webglDebugExtension.tagObject(linesRotationBuffers, 'linesRotationBuffers')
+// webglDebugExtension.tagObject(linesRotationBuffers, 'linesRotationBuffers')
 
 // Create lines VAO and bind it
 const linesVao = vaoExtension.createVertexArrayOES()
-webglDebugExtension.tagObject(planeVao, 'planeVao')
+// webglDebugExtension.tagObject(planeVao, 'planeVao')
 vaoExtension.bindVertexArrayOES(linesVao)
 
 // Lines vertices
@@ -153,7 +171,6 @@ const ballsVertexArray = new Float32Array([-GLOBAL_STATE.radius / 2, GLOBAL_STAT
 const ballsUvsArray = new Float32Array([0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1])
 
 let offsetsArray = new Float32Array(GLOBAL_STATE.particleCount * 2)
-let oldOffsetsArray = new Float32Array(GLOBAL_STATE.particleCount * 2)
 let velocitiesArray = new Float32Array(GLOBAL_STATE.particleCount * 2).fill(0)
 
 for (let i = 0; i < GLOBAL_STATE.particleCount; i++) {
@@ -161,8 +178,6 @@ for (let i = 0; i < GLOBAL_STATE.particleCount; i++) {
   const randY = Math.random() * -GLOBAL_STATE.innerHeight
   offsetsArray[i * 2 + 0] = randX
   offsetsArray[i * 2 + 1] = randY
-  oldOffsetsArray[i * 2 + 0] = randX
-  oldOffsetsArray[i * 2 + 1] = randY
 
   velocitiesArray[i * 2] = (Math.random() * 2 - 1) * 0.25
   velocitiesArray[i * 2 + 1] = Math.random()
@@ -170,15 +185,15 @@ for (let i = 0; i < GLOBAL_STATE.particleCount; i++) {
 
 // Create balls buffers
 const ballsVertexBuffer = gl.createBuffer()
-webglDebugExtension.tagObject(ballsVertexBuffer, 'ballsVertexBuffer')
+// webglDebugExtension.tagObject(ballsVertexBuffer, 'ballsVertexBuffer')
 const ballsUvsBuffer = gl.createBuffer()
-webglDebugExtension.tagObject(ballsUvsBuffer, 'ballsUvsBuffer')
+// webglDebugExtension.tagObject(ballsUvsBuffer, 'ballsUvsBuffer')
 const ballsOffsetsBuffer = gl.createBuffer()
-webglDebugExtension.tagObject(ballsOffsetsBuffer, 'ballsOffsetsBuffer')
+// webglDebugExtension.tagObject(ballsOffsetsBuffer, 'ballsOffsetsBuffer')
 
 // Create balls VAO
 const ballsVao = vaoExtension.createVertexArrayOES()
-webglDebugExtension.tagObject(ballsVao, 'ballsVao')
+// webglDebugExtension.tagObject(ballsVao, 'ballsVao')
 vaoExtension.bindVertexArrayOES(ballsVao)
 
 // Lookup balls attributes
@@ -242,7 +257,6 @@ gl.bindTexture(gl.TEXTURE_2D, null)
 document.addEventListener('DOMContentLoaded', init)
 
 function init() {
-  threadChooserWrapper.addEventListener('click', onThreadChoose)
   appContainer.appendChild(canvas)
 
   resizeCanvas()
@@ -281,42 +295,13 @@ function init() {
   gl.uniform2f(u_textTextureResolution, textureCanvas.width, textureCanvas.height)
   gl.useProgram(null)
 
-  // worker.postMessage({
-  //   type: EVT_INIT_WORLD,
-  //   innerWidth: GLOBAL_STATE.innerWidth,
-  //   innerHeight: GLOBAL_STATE.innerHeight,
-  //   radius: GLOBAL_STATE.radius,
-  //   particlesCount: GLOBAL_STATE.particleCount,
-  //   bounceScale: GLOBAL_STATE.bounceScale,
-  //   gravity: GLOBAL_STATE.gravity,
-  // })
-
-  // worker.onmessage = onWorkerMessage
   requestAnimationFrame(renderFrame)
 }
 
-// function onWorkerMessage(e) {
-//   if (!GLOBAL_STATE.useWorker) {
-//     return
-//   }
-//   if (e.data.type === EVT_UPDATED_WORLD) {
-//     const {
-//       velocitiesArray: newVelocitiesArray,
-//       offsetsArray: newOffsetsArray,
-//       oldOffsetsArray: newOldOffsetsArray,
-//     } = e.data
+function renderFrame(ts) {
+  const dt = ts - oldTime
+  oldTime = ts
 
-//     velocitiesArray = newVelocitiesArray
-//     offsetsArray = newOffsetsArray
-//     oldOffsetsArray = newOldOffsetsArray
-
-//     gl.bindBuffer(gl.ARRAY_BUFFER, offsetsBuffer)
-//     gl.bufferData(gl.ARRAY_BUFFER, offsetsArray, gl.DYNAMIC_DRAW)
-//     gl.bindBuffer(gl.ARRAY_BUFFER, null)
-//   }
-// }
-
-function renderFrame() {
   gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
 
   gl.clearColor(0.1, 0.1, 0.1, 1.0)
@@ -362,64 +347,198 @@ function renderFrame() {
   gl.useProgram(null)
   vaoExtension.bindVertexArrayOES(null)
 
-  if (GLOBAL_STATE.useWorker) {
-    if (velocitiesArray.buffer.byteLength && offsetsArray.buffer.byteLength && oldOffsetsArray.buffer.byteLength) {
-      // worker.postMessage({
-      //   type: EVT_REQUEST_UPDATE_WORLD,
-      //   velocitiesArray,
-      //   offsetsArray,
-      //   oldOffsetsArray,
-      // }, [
-      //   velocitiesArray.buffer,
-      //   offsetsArray.buffer,
-      //   oldOffsetsArray.buffer
-      // ])
+  for (let i = 0; i < GLOBAL_STATE.particleCount; i++) {
+    for (let n = i + 1; n < GLOBAL_STATE.particleCount; n++) {
+      // checkCollision(i, n)
     }
-  } else {
-    // const {
-    //   velocitiesArray: newVelocitiesArray,
-    //   offsetsArray: newOffsetsArray,
-    //   oldOffsetsArray: newOldOffsetsArray,
-    // } = 
-    calculatePhysics({
-      velocitiesArray,
-      oldOffsetsArray,
-      offsetsArray,
-      linesOffsetsArray,
-      linesRotationsArray,
-      linesVertexArray,
-    }, {
-      innerWidth: GLOBAL_STATE.innerWidth,
-      innerHeight: GLOBAL_STATE.innerHeight,
-      radius: GLOBAL_STATE.radius,
-      particlesCount: GLOBAL_STATE.particleCount,
-      bounceScale: GLOBAL_STATE.bounceScale,
-      gravity: GLOBAL_STATE.gravity,
-    })
-
-    // velocitiesArray = newVelocitiesArray
-    // offsetsArray = newOffsetsArray
-    // oldOffsetsArray = newOldOffsetsArray
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, ballsOffsetsBuffer)
-    gl.bufferData(gl.ARRAY_BUFFER, offsetsArray, gl.DYNAMIC_DRAW)
-    gl.bindBuffer(gl.ARRAY_BUFFER, null)
   }
+
+  for (let i = 0; i < GLOBAL_STATE.particleCount; i++) {
+    moveBall(dt, i)
+    checkWall(i)
+  }
+
+  for (let i = 0; i < GLOBAL_STATE.linesCount; i++) {
+    checkLine(i)
+  }
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, ballsOffsetsBuffer)
+  gl.bufferData(gl.ARRAY_BUFFER, offsetsArray, gl.DYNAMIC_DRAW)
+  gl.bindBuffer(gl.ARRAY_BUFFER, null)
 
   requestAnimationFrame(renderFrame)
 }
 
 // ------- Helpers -------
 
-function onThreadChoose(e) {
-  if (e.target.nodeName === 'BUTTON') {
-    for (let i = 0; i < threadChooserWrapper.children.length; i++) {
-      const btn = threadChooserWrapper.children[i]
-      btn.classList.remove('active')
+const getLineBounds = i => {
+  const x1 = linesVertexArray[0]
+  const y1 = linesVertexArray[1]
+  const x2 = linesVertexArray[2]
+  const y2 = linesVertexArray[3]
+  if (linesRotationsArray[i] === 0) {
+    const minX = Math.min(x1, x2)
+    const minY = Math.min(y1, y2)
+    const maxX = Math.max(x1, x2)
+    const maxY = Math.max(y1, y2)
+    return {
+      x: x1 + minX,
+      y: y1 + minY,
+      width: maxX - minX,
+      height: maxY - minY,
     }
-    const { option } = e.target.dataset
-    e.target.classList.add('active')
-    GLOBAL_STATE.useWorker = option === 'worker-thread'
+  } else {
+    const rotation = linesRotationsArray[i]
+    const sin = Math.sin(rotation)
+    const cos = Math.cos(rotation)
+    const x1r = cos * x1 + sin * y1
+    const x2r = cos * x2 + sin * y2
+    const y1r = cos * y1 + sin * x1
+    const y2r = cos * y2 + sin * x2
+    const x = linesOffsetsArray[i * 2 + 0] + x1 + Math.min(x1r, x2r)
+    const y = linesOffsetsArray[i * 2 + 1] + y1 + Math.min(y1r, y2r)
+    const width = Math.max(x1r, x2r) - Math.min(x1r, x2r)
+    const height = Math.max(y1r, y2r) - Math.min(y1r, y2r)
+    return {
+      x,
+      y,
+      width,
+      height,
+    }
+  }
+}
+
+function moveBall(dt, i) {
+  velocitiesArray[i * 2 + 1] += GLOBAL_STATE.gravity
+  offsetsArray[i * 2 + 0] += velocitiesArray[i * 2 + 0] * (dt)
+  offsetsArray[i * 2 + 1] += velocitiesArray[i * 2 + 1] * (dt)
+}
+
+function checkWall(i) {
+  if (offsetsArray[i * 2 + 0] < GLOBAL_STATE.particleCount / 2) {
+    offsetsArray[i * 2] = GLOBAL_STATE.particleCount / 2
+    velocitiesArray[i * 2] *= -GLOBAL_STATE.bounceScale
+  }
+  if (offsetsArray[i * 2] + GLOBAL_STATE.radius / 2 > innerWidth) {
+    offsetsArray[i * 2] = innerWidth - GLOBAL_STATE.radius / 2
+    velocitiesArray[i * 2] *= -GLOBAL_STATE.bounceScale
+  }
+  if (offsetsArray[i * 2 + 1] - GLOBAL_STATE.particleCount / 2 > innerHeight) {
+    offsetsArray[i * 2 + 1] = -GLOBAL_STATE.particleCount
+    velocitiesArray[i * 2 + 1] = 0
+  }
+}
+
+function checkLine(lineIdx) {
+  const lineBounds = getLineBounds(lineIdx)
+  const ballRadius = GLOBAL_STATE.particleCount / 1.5
+
+  for (let i = 0; i < GLOBAL_STATE.particleCount; i++) {
+    const ballx = offsetsArray[i * 2 + 0]
+    const bally = offsetsArray[i * 2 + 1]
+    const ballvx = velocitiesArray[i * 2 + 0]
+    const ballvy = velocitiesArray[i * 2 + 1]
+
+    if (ballx + ballRadius / 2 > lineBounds.x && ballx - ballRadius / 2 < lineBounds.x + lineBounds.width) {
+      const lineRotation = linesRotationsArray[lineIdx]
+      const cos = Math.cos(lineRotation)
+      const sin = Math.sin(lineRotation)
+
+      let x = ballx - linesOffsetsArray[lineIdx * 2 + 0]
+      let y = bally - linesOffsetsArray[lineIdx * 2 + 1]
+      let vx1 = cos * ballvx + sin * ballvy
+      let vy1 = cos * ballvy - sin * ballvx
+
+      let y1 = cos * y - sin * x
+
+      if (y1 > -ballRadius / 2 && y1 < vy1) {
+        const x2 = cos * x + sin * y
+
+        y1 = -ballRadius / 2
+        vy1 *= -0.35
+
+        x = cos * x2 - sin * y1
+        y = cos * y1 + sin * x2
+
+        velocitiesArray[i * 2 + 0] = cos * vx1 - sin * vy1
+        velocitiesArray[i * 2 + 1] = cos * vy1 + sin * vx1
+
+        offsetsArray[i * 2 + 0] = linesOffsetsArray[lineIdx * 2 + 0] + x
+        offsetsArray[i * 2 + 1] = linesOffsetsArray[lineIdx * 2 + 1] + y
+      }
+    }
+  }
+}
+
+function rotate(x, y, sin, cos, reverse) {
+  return {
+    x: reverse ? x * cos + y * sin : x * cos - y * sin,
+    y: reverse ? y * cos - x * sin : y * cos + x * sin
+  }
+}
+
+function checkCollision(i0, i1) {
+  const ballMass = 1
+  const ball0x = offsetsArray[i0 * 2 + 0]
+  const ball0y = offsetsArray[i0 * 2 + 1]
+  const ball1x = offsetsArray[i1 * 2 + 0]
+  const ball1y = offsetsArray[i1 * 2 + 1]
+
+  let ball0vx = velocitiesArray[i0 * 2 + 0]
+  let ball0vy = velocitiesArray[i0 * 2 + 1]
+  let ball1vx = velocitiesArray[i1 * 2 + 0]
+  let ball1vy = velocitiesArray[i1 * 2 + 1]
+
+  const dx = ball1x - ball0x
+  const dy = ball1y - ball0y
+  const dist = Math.sqrt(dx * dx + dy * dy)
+  const minDist = GLOBAL_STATE.radius + GLOBAL_STATE.radius
+  if (dist < minDist) {
+    //calculate angle, sine, and cosine
+    const angle = Math.atan2(dy, dx)
+    const sin = Math.sin(angle)
+    const cos = Math.cos(angle)
+
+    //rotate ball0's position
+    const pos0 = { x: 0, y: 0 }
+
+    //rotate ball1's position
+    const pos1 = rotate(dx, dy, sin, cos, true)
+
+    //rotate ball0's velocity
+    const vel0 = rotate(ball0vx, ball0vy, sin, cos, true)
+
+    //rotate ball1's velocity
+    const vel1 = rotate(ball1vx, ball1vy, sin, cos, true)
+
+    //collision reaction
+    const vxTotal = (vel0.x - vel1.x)
+    vel0.x = ((ballMass - ballMass) * vel0.x + 2 * ballMass * vel1.x) / (ballMass + ballMass)
+    vel1.x = vxTotal + vel0.x
+
+    const absV = Math.abs(vel0.x) + Math.abs(vel1.x)
+    const overlap = (GLOBAL_STATE.radius + GLOBAL_STATE.radius) - Math.abs(pos0.x - pos1.x)
+    pos0.x += vel0.x / absV * overlap
+    pos1.x += vel1.x / absV * overlap
+
+    //rotate positions back
+    const pos0F = rotate(pos0.x, pos0.y, sin, cos, false)
+    const pos1F = rotate(pos1.x, pos1.y, sin, cos, false)
+
+    //adjust positions to actual screen positions
+    offsetsArray[i1 * 2 + 0] = offsetsArray[i0 * 2 + 0] + pos1F.x
+    offsetsArray[i1 * 2 + 1] = offsetsArray[i0 * 2 + 1] + pos1F.y
+    offsetsArray[i0 * 2 + 0] = offsetsArray[i0 * 2 + 0] + pos0F.x
+    offsetsArray[i0 * 2 + 1] = offsetsArray[i0 * 2 + 1] + pos0F.y
+
+    //rotate velocities back
+    const vel0F = rotate(vel0.x, vel0.y, sin, cos, false)
+    const vel1F = rotate(vel1.x, vel1.y, sin, cos, false)
+
+    velocitiesArray[i0 * 2 + 0] = vel0F.x
+    velocitiesArray[i0 * 2 + 1] = vel0F.y
+    velocitiesArray[i1 * 2 + 0] = vel1F.x
+    velocitiesArray[i1 * 2 + 1] = vel1F.y
   }
 }
 
